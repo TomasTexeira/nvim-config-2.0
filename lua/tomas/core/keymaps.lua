10c839434a3952dc2bd05f5ed9989ae0f85d40ca
TomasTexeira <tom.tex2322@gmail.com>
2022-11-24 16:02:44 -0300

    first version

---
+++ lua/tomas/core/keymaps.lua
@@ -0,0 +1,51 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- Do not yank with x
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+-- shift + =
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Delete a word backwards
+-- dw
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+-- control + a
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- New tab
+keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('', 's<left>', '<C-w>h')
+keymap.set('', 's<up>', '<C-w>k')
+keymap.set('', 's<down>', '<C-w>j')
+keymap.set('', 's<right>', '<C-w>l')
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sk', '<C-w>k')
+keymap.set('', 'sj', '<C-w>j')
+keymap.set('', 'sl', '<C-w>l')
+
+-- Resize window
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
---
+++ lua/tomas/plugins/lsp/mason.lua
@@ -0,0 +1,42 @@
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+  return
+end
+
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+  return
+end
+
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+  return
+end
+
+mason.setup()
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    "arduino_language_server",
+    "bashls",
+    "clangd",
+    "csharp_ls",
+    "clangd",
+    "cssls",
+    "html",
+    "jsonls",
+    "quick_lint_js",
+    "tsserver",
+    "pylsp",
+    "tailwindcss",
+  }
+})
+
+mason_null_ls.setup({
+  ensure_installed = {
+    "prettier",
+    "stylua",
+    "eslint_d",
+    "rome",
+  }
+})
